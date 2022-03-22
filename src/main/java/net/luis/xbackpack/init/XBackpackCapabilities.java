bfdae8b164816170121e5ec29a1dab93a749a213
Luis <76595940+Luis-st@users.noreply.github.com>
2022-03-22 20:41:31 +0100

    fix critical issue and update to version 1.18.2-1.5

--- src/main/java/net/luis/xbackpack/init/XBackpackCapabilities.java
+++ src/main/java/net/luis/xbackpack/init/XBackpackCapabilities.java
@@ -3,6 +3,8 @@ package net.luis.xbackpack.init;
 import net.luis.xbackpack.XBackpack;
 import net.luis.xbackpack.common.capability.IBackpack;
 import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.CapabilityManager;
+import net.minecraftforge.common.capabilities.CapabilityToken;
 
 /**
  * registration class, for the {@link XBackpack} Capability
@@ -15,6 +17,6 @@ public class XBackpackCapabilities {
 	/**
 	 * {@link IBackpack} Capability
 	 */
-	public static Capability<IBackpack> BACKPACK;
+	public static final Capability<IBackpack> BACKPACK = CapabilityManager.get(new CapabilityToken<IBackpack>() {});
 	
 }
