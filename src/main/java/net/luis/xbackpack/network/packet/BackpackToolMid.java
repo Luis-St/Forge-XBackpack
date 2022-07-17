25a9876fea43b70924f6f73b3f8c8dc4595da07e
Luis <76595940+Luis-st@users.noreply.github.com>
2022-07-17 20:25:14 +0200

    add tool slot mid network packet

---
+++ src/main/java/net/luis/xbackpack/network/packet/BackpackToolMid.java
@@ -0,0 +1,41 @@
+package net.luis.xbackpack.network.packet;
+
+import java.util.function.Supplier;
+
+import net.luis.xbackpack.BackpackConstans;
+import net.luis.xbackpack.world.capability.IBackpack;
+import net.luis.xbackpack.world.capability.XBackpackCapabilities;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.network.NetworkEvent.Context;
+
+public class BackpackToolMid {
+	
+	public BackpackToolMid() {
+		
+	}
+	
+	public BackpackToolMid(FriendlyByteBuf buffer) {
+		
+	}
+	
+	public void encode(FriendlyByteBuf buffer) {
+		
+	}
+	
+	public void handle(Supplier<Context> context) {
+		ServerPlayer player = context.get().getSender();
+		context.get().enqueueWork(() -> {
+			IBackpack backpack = player.getCapability(XBackpackCapabilities.BACKPACK, null).orElseThrow(NullPointerException::new);
+			ItemStack main = player.getMainHandItem().copy();
+			ItemStack down = backpack.getStackInSlot(BackpackConstans.BACKPACK_TOOL_SLOT_MID).copy();
+			if (BackpackConstans.VALID_TOOL_SLOT_ITEMS.contains(main.getItem())) {
+				player.setItemInHand(InteractionHand.MAIN_HAND, down);
+				backpack.setStackInSlot(BackpackConstans.BACKPACK_TOOL_SLOT_MID, main);
+			}
+		});
+	}
+	
+}
