46718e41700502061ef5fc1470f2b13dff830e8a
Luis <76595940+Luis-st@users.noreply.github.com>
2022-08-29 22:37:07 +0200

    added some more features to BackpackCommand

--- src/main/java/net/luis/xbackpack/jei/BackpackContainerHandler.java
+++ src/main/java/net/luis/xbackpack/jei/BackpackContainerHandler.java
@@ -6,8 +6,10 @@ import com.google.common.collect.Lists;
 
 import mezz.jei.api.gui.handlers.IGuiContainerHandler;
 import net.luis.xbackpack.client.gui.screens.BackpackScreen;
+import net.luis.xbackpack.world.capability.BackpackProvider;
 import net.luis.xbackpack.world.extension.BackpackExtension;
 import net.luis.xbackpack.world.extension.BackpackExtensions;
+import net.luis.xbackpack.world.extension.ExtensionState;
 import net.minecraft.client.renderer.Rect2i;
 
 /**
@@ -19,13 +21,17 @@ import net.minecraft.client.renderer.Rect2i;
 public class BackpackContainerHandler implements IGuiContainerHandler<BackpackScreen> {
 	
 	@Override
+	@SuppressWarnings("resource")
 	public List<Rect2i> getGuiExtraAreas(BackpackScreen screen) {
-		List<Rect2i> extraAreas = Lists.newArrayList(new Rect2i(screen.getGuiLeft() + screen.getXSize(), screen.getGuiTop(), 30, screen.getYSize()));
-		BackpackExtension extension = screen.getExtension();
-		if (extension != BackpackExtensions.NO.get()) {
-			extraAreas.add(new Rect2i(screen.getGuiLeft() + screen.getXSize(), screen.getGuiTop() +  screen.getExtensionOffset(extension), extension.getImageWidth(), extension.getImageHeight()));
+		if (BackpackProvider.get(screen.getMinecraft().player).getConfig().getWithState(ExtensionState.UNLOCKED).size() > 0) {
+			List<Rect2i> extraAreas = Lists.newArrayList(new Rect2i(screen.getGuiLeft() + screen.getXSize(), screen.getGuiTop(), 30, screen.getYSize()));
+			BackpackExtension extension = screen.getExtension();
+			if (extension != BackpackExtensions.NO.get()) {
+				extraAreas.add(new Rect2i(screen.getGuiLeft() + screen.getXSize(), screen.getGuiTop() +  screen.getExtensionOffset(extension), extension.getImageWidth(), extension.getImageHeight()));
+			}
+			return extraAreas;
 		}
-		return extraAreas;
+		return Lists.newArrayList();
 	}
 	
 }
--- src/main/java/net/luis/xbackpack/server/commands/BackpackCommand.java
+++ src/main/java/net/luis/xbackpack/server/commands/BackpackCommand.java
@@ -10,6 +10,7 @@ import net.luis.xbackpack.server.commands.arguments.ExtensionStateArgument;
 import net.luis.xbackpack.world.capability.BackpackProvider;
 import net.luis.xbackpack.world.capability.IBackpack;
 import net.luis.xbackpack.world.extension.BackpackExtension;
+import net.luis.xbackpack.world.extension.BackpackExtensions;
 import net.luis.xbackpack.world.extension.ExtensionState;
 import net.minecraft.commands.CommandBuildContext;
 import net.minecraft.commands.CommandSourceStack;
@@ -29,36 +30,48 @@ public class BackpackCommand {
 	public static void register(CommandDispatcher<CommandSourceStack> dispatcher, CommandBuildContext context) {
 		dispatcher.register(Commands.literal("backpack").requires((source) -> {
 			return source.hasPermission(2);
-		}).then(Commands.argument("players", EntityArgument.players()).then(Commands.argument("extension", BackpackExtensionArgument.extension()).then(Commands.literal("get").executes((command) -> {
-			return getExtensionState(command.getSource(), Lists.newArrayList(EntityArgument.getPlayers(command, "players")), BackpackExtensionArgument.get(command, "extension"));
-		})).then(Commands.literal("set").then(Commands.argument("state", ExtensionStateArgument.state()).executes((command) -> {
-			return setExtensionState(command.getSource(), Lists.newArrayList(EntityArgument.getPlayers(command, "players")), BackpackExtensionArgument.get(command, "extension"), ExtensionStateArgument.get(command, "state"));
-		}))))));
+		}).then(Commands.argument("player", EntityArgument.player()).then(Commands.literal("*").then(Commands.argument("state", ExtensionStateArgument.state()).executes((command) -> {
+			return setExtensionState(command.getSource(), EntityArgument.getPlayer(command, "player"), Lists.newArrayList(BackpackExtensions.REGISTRY.get().getValues()), ExtensionStateArgument.get(command, "state"));
+		}))).then(Commands.argument("extension", BackpackExtensionArgument.extension()).executes((command) -> {
+			return getExtensionState(command.getSource(), EntityArgument.getPlayer(command, "player"), Lists.newArrayList(BackpackExtensionArgument.get(command, "extension")));
+		}).then(Commands.argument("state", ExtensionStateArgument.state()).executes((command) -> {
+			return setExtensionState(command.getSource(), EntityArgument.getPlayer(command, "player"), Lists.newArrayList(BackpackExtensionArgument.get(command, "extension")), ExtensionStateArgument.get(command, "state"));
+		})))));
 	}
 	
-	private static int getExtensionState(CommandSourceStack source, List<ServerPlayer> players, BackpackExtension extension) {
-		if (players.size() > 1) {
-			source.sendFailure(Component.translatable("xbackpack.commands.backpack.get_failure"));
+	private static int getExtensionState(CommandSourceStack source, ServerPlayer player, List<BackpackExtension> extensions) {
+		if (extensions.size() > 1) {
+			source.sendFailure(Component.translatable("xbackpack.commands.backpack.get_failure", player.getName().getString()));
 		} else {
-			ServerPlayer player = players.get(0);
-			source.sendSuccess(Component.translatable("xbackpack.commands.backpack.get_success", extension, player.getName().getString(), BackpackProvider.get(player).getConfig().getState(extension).getName()), false);
+			source.sendSuccess(Component.translatable("xbackpack.commands.backpack.get_success", getName(extensions.get(0)), player.getName().getString(), BackpackProvider.get(player).getConfig().getState(extensions.get(0)).getName()), false);	
 		}
 		return 1;
 	}
 	
-	private static int setExtensionState(CommandSourceStack source, List<ServerPlayer> players, BackpackExtension extension, ExtensionState state) {
+	private static int setExtensionState(CommandSourceStack source, ServerPlayer player, List<BackpackExtension> extensions, ExtensionState state) {
 		int i = 0;
-		for (ServerPlayer player : players) {
-			IBackpack backpack = BackpackProvider.get(player);
-			if (backpack.getConfig().setState(extension, state)) {
-				++i;
-			}
-			backpack.broadcastChanges();
+		IBackpack backpack = BackpackProvider.get(player);
+		for (BackpackExtension extension : extensions) {
+			backpack.getConfig().setState(extension, state);
+			++i;
 		}
-		if (players.size() > 0) {
-			source.sendSuccess(Component.translatable("xbackpack.commands.backpack.set_success", extension, i, state.getName()), false);
+		backpack.broadcastChanges();
+		if (i == 1) {
+			source.sendSuccess(Component.translatable("xbackpack.commands.backpack.set_success.single", getName(extensions.get(0)), player.getName().getString(), state.getName()), false);
+		} else if (i > 1) {
+			source.sendSuccess(Component.translatable("xbackpack.commands.backpack.set_success.multiple", i, player.getName().getString(), state.getName()), false);
 		}
 		return 1;
+	}
+	
+	private static String getName(BackpackExtension extension) { 
+		String[] nameParts = BackpackExtensions.REGISTRY.get().getKey(extension).getPath().split("_");
+		String name = "";
+		for (String namePart : nameParts) {
+			String startChar = namePart.substring(0, 1).toUpperCase();
+			name += startChar + namePart.substring(1, namePart.length()) + " ";
+		}
+		return name.trim();
 	}
 	
 }
